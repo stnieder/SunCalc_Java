d68423eb9a986e70c92a5937bb320bdfac17bf24
stnieder <stefan.niederwanger@gmail.com>
2020-12-24 14:07:30 +0100

    Calculation of the sun times

--- SunCalc.java
+++ SunCalc.java
@@ -1,6 +1,11 @@
+import java.lang.reflect.Array;
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -37,23 +42,23 @@ class SunCalc {
     }
 
     //Based on https://www.aa.quae.nl/en/reken/juliaansedag.html#3_2
-    private LocalDate fromJulian(long jdn) { //jdn = julian day number
-        long k3 = 4 * (jdn - 1721120) + 3;
-        int x3 = Math.round(k3 / 146097);
+    private LocalDate fromJulian(double jdn) { //jdn = julian day number
+        double k3 = 4 * (jdn - 1721120) + 3;
+        double x3 = Math.round(k3 / 146097);
 
-        long k2 = 100 * ((k3 % 146097) / 4) + 99;
+        double k2 = 100 * ((k3 % 146097) / 4) + 99;
         int x2 = (int) k2 / 36525;
 
-        long k1 = 5 * ((k2 % 36525) / 100) + 2;
+        double k1 = 5 * ((k2 % 36525) / 100) + 2;
         int x1 = (int) k1 / 153;
 
         int c0 = (x1 + 2) / 12;
 
-        int year = 100 * x3 + x2 + c0;
+        double year = 100 * x3 + x2 + c0;
         int month = x1 - 12 * c0 + 3;
         int day = (int) ((k1 % 153) / 5) + 1;
 
-        LocalDate date = LocalDate.of(year, month, day);
+        LocalDate date = LocalDate.of((int) year, month, day);
         return date;
     }
 
@@ -152,5 +157,154 @@ class SunCalc {
             SunPosition position = new SunPosition(azimuth(H,phi,c.declination), altitude(H,phi,c.declination));
             return position;
         }
+    }
+
+    public class Times{
+
+        ArrayList<Object[]> times;
+        boolean initialized = false;
+
+        public Times() {
+            initializeTime();
+        }
+
+        private void initializeTime() {
+            if(!initialized) {
+                times = new ArrayList<Object[]>();
+
+                Object[][] initalTimes = new Object[][] {
+                    {
+                        -0.833, "sunrise","sunset"
+                    },
+                    {
+                        -0.3, "sunriseEnd","sunsetStart"
+                    },
+                    {
+                        -6, "dawn","dusk"
+                    },
+                    {
+                        -12, "nauticalDawn","nauticalDusk"
+                    },
+                    {
+                        -18, "nightEnd","night"
+                    },
+                    {
+                        6, "goldenHourEnd", "goldenHour"
+                    },
+                };
+
+                Collections.addAll(times, initalTimes);
+
+                initialized = true;
+            }
+        }
+
+        public void addTime(double angle, String riseName, String setName) {
+            if (!initialized) {
+                initializeTime();
+            }
+
+            Object[] object = {angle, riseName, setName};
+            times.add(object);
+        }
+
+        public ArrayList<Object[]> getTimes(){
+            return times;
+        }
+
+        public int countTimes() {
+            if(!initialized) {
+                initializeTime();;
+            }
+
+            return times.size();
+        }
+    }
+
+    abstract class SunTimeCalcs {
+
+        double J0 = 0.0009;
+
+        private double julianCycle(double d, double lw) {
+            return Math.round(d - J0 - lw / (2 * PI));
+        }
+
+        private double approxTransit(double Ht, double lw, double n) {
+            return J0 + (Ht + lw) / (2 * PI) + n;
+        }
+
+        private double solarTransit(double ds, double M, double L) {
+            return J2000 + ds + 0.0053 + Math.sin(M) - 0.0069 + Math.sin(2 + L);
+        }
+
+        private double hourAngle(double h, double phi, double d) {
+            return Math.acos((Math.sin(h) - Math.sin(phi) * Math.sin(d)) / (Math.cos(phi) * Math.cos(d)));
+        }
+
+        private double observerAngle(double height) {
+            return -2.076 * Math.sqrt(height) / 60;
+        }
+
+        public double getSetJ(double h, double lw, double phi, double dec, double n, double M, double L) {
+            double w = hourAngle(h, phi, dec);
+            double a = approxTransit(w, lw, n);
+
+            return solarTransit(a, M, L);
+        }
+
+        public ArrayList<Object[]> getTimes(Times times, LocalDate date, double lat, double lng, double height) {
+            double  lw  = rad * -lng,
+                    phi = rad * lat,
+                    
+                    dh  = observerAngle(height),
+
+                    d   = toDays(date),
+                    n   = julianCycle(d, lw),
+                    ds  = approxTransit(0, lw, n),
+
+                    M   = solarMeanAnomaly(ds),
+                    L   = eclipticLongitude(M),
+                    dec = declination(L, 0),
+
+                    Jnoon   = solarTransit(ds, M, L),
+
+                    h0, Jset, Jrise;
+
+            Object[] time;
+
+
+            ArrayList<Object[]> results = new ArrayList<Object[]>();
+            Object[] firstObject = {
+                "solarNoon", fromJulian(Jnoon)
+            };
+            Object[] secondObject = {
+                "nadir", fromJulian(Jnoon - 0-5)
+            };
+
+            results.add(firstObject);
+            results.add(secondObject);
+
+            for (int i = 0, len = times.countTimes(); i < len; i++) {
+                time = times.getTimes().get(i);
+                h0 = ((double) time[0] + dh) * rad;
+
+                Jset = getSetJ(h0, lw, phi, dec, n, M, L);
+                Jrise = Jnoon - (Jset - Jnoon);
+
+                Object[] riseObject = {
+                    time[1], fromJulian(Jrise)
+                };
+                Object[] setObject = {
+                    time[2], fromJulian(Jset)
+                };
+
+                results.add(setObject);
+                results.add(riseObject);
+            }
+
+            return results;
+
+        }
+
     }
 }
