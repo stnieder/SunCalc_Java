9b1dc225c4b7fe40a14afb9db5d51bc97246541b
stnieder <stefan.niederwanger@gmail.com>
2020-12-27 01:03:22 +0100

    debugging javascript

--- suncalc.js
+++ suncalc.js
@@ -27,7 +27,7 @@ var dayMs = 1000 * 60 * 60 * 24,
     J2000 = 2451545;
 
 function toJulian(date) { return date.valueOf() / dayMs - 0.5 + J1970; }
-function fromJulian(j)  { return new Date((j + 0.5 - J1970) * dayMs); }
+function fromJulian(j)  { console.log("fromJulian.jdn: "+(j+0.5-J1970) * dayMs); return new Date((j + 0.5 - J1970) * dayMs); }
 function toDays(date)   { return toJulian(date) - J2000; }
 
 
@@ -160,6 +160,7 @@ SunCalc.getTimes = function (date, lat, lng, height) {
 
         i, len, time, h0, Jset, Jrise;
 
+    console.log("getTimes.Jnoon: " + Jnoon);
 
     var result = {
         solarNoon: fromJulian(Jnoon),
